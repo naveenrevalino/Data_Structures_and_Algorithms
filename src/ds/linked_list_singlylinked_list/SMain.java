f950b9eaa1e05da45a30f2c95818cf08ee69f483
Naveen Ravi <naveen.revalino@gmail.com>
2017-12-01 10:00:41 -0500

    4. Singly Linked List

--- src/ds/linked_list_singlylinked_list/SMain.java
+++ src/ds/linked_list_singlylinked_list/SMain.java
@@ -1,15 +1,15 @@
 package ds.linked_list_singlylinked_list;
+import ds.linked_list_singlylinked_list.SinglyLinkedList;
 
-public class Main {
+public class SMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SinglyLinkedList mylist = new SinglyLinkedList();
-		mylist.insertFirst(100);
-		mylist.insertFirst(99);
-		mylist.insertFirst(88);
-		mylist.insertFirst(77);
-		mylist.insertLast(99999);
+		mylist.insertFirst(10);
+		mylist.insertFirst(20);
+		mylist.insertFirst(30);
+		mylist.insertLast(999);
 		mylist.display();
 
 	}
